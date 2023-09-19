affa5a1d5f9471207247eeb359ddb21cf8670568
Yout <youtlone95@gmail.com>
2023-09-19 19:35:43 +0630

    add user post comment and like method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,13 @@
 class Comment < ApplicationRecord
-    belongs_to :post, class_name: 'Post'
-    belongs_to :author, class_name: 'User'
-  
-    after_create :update_comments_counter
-    after_destroy :update_comments_counter
-  
-    private
-  
-    def update_comments_counter
-      post.update(comment_counter: post.comments.count)
-    end
+  belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comment_counter: post.comments.count)
   end
-  
+end
